4b567841f95e0ac4ea8c26e37392a0af87bc1ea6
Max <grimfire@gmail.com>
2017-03-11 16:01:00 +0300

    +

---
+++ Events/Controls/ControlsInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TiqUtils.Events.Controls
+{
+    public class ControlsInput
+    {
+        private static bool IsTextAllowed(string text)
+        {
+            var regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+            return !regex.IsMatch(text);
+        }
+
+        public static bool OnlyNum(char ch)
+        {
+            if (!char.IsControl(ch)
+                && !char.IsDigit(ch))
+            {
+                return true;
+            }
+            return ch == ',' || ch == '.';
+        }
+    }
+}
