7fd876c30f5ab59a7e9ab3440ec4c8941ebe5081
Max <grimfire@gmail.com>
2017-05-21 22:31:16 +0300

    data storage

---
+++ Data/InMemoryDataStorage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using TiqUtils.TypeSpeccific;
+
+namespace TiqUtils.Data
+{
+    public static class InMemoryDataStorage
+    {
+        private static readonly Dictionary<string, object> Storage = new Dictionary<string, object>();
+
+        public static string Store<T>(T data)
+        {
+            var newKey = StringUtils.RandomString(16);
+            if (Storage.ContainsKey(newKey))
+                return Store(data);
+            Storage.Add(newKey, data);
+            return newKey;
+        }
+
+        public static T GetData<T>(string key)
+        {
+            if (Storage.ContainsKey(key))
+                return (T)Storage[key];
+            return default(T);
+        }
+    }
+}
