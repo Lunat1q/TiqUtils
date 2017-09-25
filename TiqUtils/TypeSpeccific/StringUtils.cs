de4ec2cd5ebf438997c68e1600d7c266c5c745d2
Max <grimfire@gmail.com>
2017-09-25 18:13:36 +0300

    new string utils method
    - compute how far is each word from another

--- TiqUtils/TypeSpeccific/StringUtils.cs
+++ TiqUtils/TypeSpeccific/StringUtils.cs
@@ -135,6 +135,43 @@ namespace TiqUtils.TypeSpeccific
             return false;
         }
 
+        public static int ComputeStringDifference(this string s, string t, bool ignoreCase = false)
+        {
+            if (ignoreCase)
+            {
+                s = s.ToLower();
+                t = t.ToLower();
+            }
+            int n = s.Length, m = t.Length;
+            var d = new int[n + 1, m + 1];
+
+            if (n == 0)
+                return m;
+
+            if (m == 0)
+                return n;
+
+
+            for (var i = 0; i <= n; d[i, 0] = i++)
+            {
+            }
+
+            for (var j = 0; j <= m; d[0, j] = j++)
+            {
+            }
+
+            for (var i = 1; i <= n; i++)
+            for (var j = 1; j <= m; j++)
+            {
+                var cost = t[j - 1] == s[i - 1] ? 0 : 1;
+
+                d[i, j] = Math.Min(
+                    Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                    d[i - 1, j - 1] + cost);
+            }
+            return d[n, m];
+        }
+
         public static bool Contains(this string source, string toCheck, StringComparison comp)
         {
             return source.IndexOf(toCheck, comp) >= 0;
