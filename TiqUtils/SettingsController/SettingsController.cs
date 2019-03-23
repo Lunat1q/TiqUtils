20729fc6a7b5a18e92dc3511bd7e13b95aa92178
Max <grimfire@gmail.com>
2019-03-23 15:41:02 +0300

    new features

---
+++ TiqUtils/SettingsController/SettingsController.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using TiqUtils.Serialize;
+
+namespace TiqUtils.SettingsController
+{
+    public class SettingsController<T> where T: class, new()
+    {
+        private T _settings;
+        public string StorageFolder { get; set; } = "Config";
+
+        public string ConfigFileName { get; set; } = "settings.config";
+
+        private string AssemblyDirectory { get; }
+
+        private string ConfigPath => Path.Combine(AssemblyDirectory, StorageFolder, ConfigFileName);
+
+        public T Settings
+        {
+            get => _settings ?? (_settings = Initialize());
+            private set => _settings = value;
+        }
+
+        public SettingsController(string assemblyDirectory)
+        {
+            AssemblyDirectory = assemblyDirectory;
+        }
+
+        public T Initialize()
+        {
+            return Json.DeserializeDataFromFile<T>(ConfigPath) ?? new T();
+        }
+
+        public void Save()
+        {
+            Settings.SerializeDataJson(ConfigPath);
+        }
+
+        public void Reset()
+        {
+            Settings = new T();
+        }
+    }
+}
